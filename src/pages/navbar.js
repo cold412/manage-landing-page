9d12928d3d8fc985693977df9a1c0b860d231062
cold412 <cold412+gitlab@protonmail.com>
2020-05-15 22:39:28 +0200

    navbar mobile added

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import logo from '../img/logo.svg';
+import React from "react"
+import logo from "../img/logo.svg"
+import hamburger from "../img/icon-hamburger.svg"
+import close from "../img/icon-close.svg"
 
 const Navbar = ({ data }) => {
-
-    return (
-        <div className="nav">
-            <div className="nav__logo">
-            <img src={logo} alt="Logo" />
-            </div>
-            <div className="nav__menu">
-                <ul>
-                    <li>
-                        <a href="#">Pricing</a>
-                        <a href="#">Product</a>
-                        <a href="#">About Us</a>
-                        <a href="#">Careers</a>
-                        <a href="#">Community</a>
-                    </li>
-                </ul>
-            </div>
-  Get Starte
-        </div>
-    )
+  return (
+    <div className="nav">
+      <div className="nav__logo">
+        <img src={logo} alt="Logo" />
+        <img id="ham" src={hamburger} alt="hamburger" />
+        <img id="close" src={close} alt="close" />
+      </div>
+      <div className="nav__menu">
+        <ul>
+          <li>
+            <a href="#">Pricing</a>
+            <a href="#">Product</a>
+            <a href="#">About Us</a>
+            <a href="#">Careers</a>
+            <a href="#">Community</a>
+          </li>
+        </ul>
+      </div>
+      <div className="btn">
+        <h4>Get Started</h4>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
-
-
-
-

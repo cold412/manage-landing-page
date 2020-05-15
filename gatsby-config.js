f24b9ff22ffe18945c9a5abecf98016c0d4d5746
cold412 <cold412+gitlab@protonmail.com>
2020-05-15 18:42:15 +0200

    image plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,20 +6,39 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`,
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: [`Be Vietnam`, `source sans pro\:400,500,700`],
+        display: "swap",
+      },
+    },
 
-  {
-    resolve: 'gatsby-plugin-google-fonts',
-    options:{
-      fonts:[
-        `Be Vietnam`,
-        `source sans pro\:400,500,700`
-      ],
-      display: 'swap'
-    }
-  },
-
-
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
 
 
-]}
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img/`,
+      },
+    },
+
+
+
+
+
+
+  ],
+}

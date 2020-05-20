939e431489d65ad814358473b942bb25a52d52b4
cold412 <cold412+gitlab@protonmail.com>
2020-05-20 09:13:52 +0200

    footer start

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Page2 from "./page2"
 import Page25 from "./page25"
 import Page3 from "./page3"
 import Page4 from "./page4"
+import Footer from "./footer"
 import "../styles/navbar.scss"
 import '../styles/index.scss'
 import '../styles/page1.scss'
@@ -12,6 +13,7 @@ import '../styles/page2.scss'
 import '../styles/page25.scss'
 import '../styles/page3.scss'
 import '../styles/page4.scss'
+import '../styles/footer.scss'
 
 
 
@@ -24,6 +26,7 @@ const index = () => {
             <Page25/>
             <Page3/>
             <Page4/>
+            <Footer/>
         </div>
     )
 }

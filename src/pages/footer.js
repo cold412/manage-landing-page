d3ff070f20c1a12b4c3547ed5ff61cbbd977ae33
cold412 <cold412+gitlab@protonmail.com>
2020-05-20 10:06:43 +0200

    footerstart

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            
+            <h1>Test</h1>
         </div>
     )
 }
--- src/pages/page4.js
+++ src/pages/page4.js
@@ -3,14 +3,17 @@ import React from 'react'
 const Page4 = () => {
     return (
         <div className="page4__container">
-            <div className="page4__btn">
+{            <div className="page4__btn">
 
                 Get Started
-            </div>
+            </div>}
             <div className="page4__bottom">
+                <div className="page4__text">
+
                 <h1>
                     Simplify how your team <br /> works today.
                 </h1>
+                </div>
                 <div className="page4__btn2">
                     <h3>
 
